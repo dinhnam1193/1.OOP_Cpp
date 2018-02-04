6f57d0c848782f65cab9ec5e8a48e2633cb9db69
Dinh Nam <dinhnam1193@gmail.com>
2018-02-04 15:08:52 +0700

    NamND Example_First.cc to forder Ex_1

---
+++ Ex_1/Example_First.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+class fraction
+{
+private:
+	int denominator;
+	int numerator;
+
+public:
+	fraction() {
+	}
+	fraction(int numerator, int denominator) {
+		this->denominator = denominator;
+		this->numerator = numerator;
+	}
+	void show();
+public:
+	int getDenominator() {
+		return denominator;
+	}
+	void setDenominator(int denominator) {
+		this->denominator = denominator;
+	}
+	int getNumerator() {
+		return numerator;
+	}
+	void setNumerator(int numerator) {
+		this->numerator = numerator;
+	}
+	fraction operator+(const fraction& b) {
+		fraction f;
+		f.setDenominator(this->denominator + b.denominator);
+		f.setNumerator(this->numerator + b.numerator);
+		return f;
+	}
+};
+
+void fraction::show() {
+	cout <<"show";
+}
+int main()
+{
+	fraction frac1(1, 2);
+	fraction frac2(3, 4);
+	fraction frac3 = frac1 + frac2;
+	cout << "fraction: " << frac3.getNumerator() << "/" << frac3.getDenominator();
+	
+	return 0;
+}

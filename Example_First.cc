aa6eb6ea0817af6f85a6bed806d0b9e4c0687c33
Dinh Nam <dinhnam1193@gmail.com>
2018-02-04 10:05:03 +0700

    NamND change file Example

--- Example_First.cc
+++ Example_First.cc
@@ -9,10 +9,13 @@ private:
 	int numerator;
 
 public:
+	fraction() {
+	}
 	fraction(int numerator, int denominator) {
 		this->denominator = denominator;
 		this->numerator = numerator;
 	}
+	void show();
 public:
 	int getDenominator() {
 		return denominator;
@@ -26,13 +29,23 @@ public:
 	void setNumerator(int numerator) {
 		this->numerator = numerator;
 	}
+	fraction operator+(const fraction& b) {
+		fraction f;
+		f.setDenominator(this->denominator + b.denominator);
+		f.setNumerator(this->numerator + b.numerator);
+		return f;
+	}
 };
 
+void fraction::show() {
+	
+}
 int main()
 {
 	fraction frac1(1, 2);
-	
-	cout << "fraction: " << frac1.getNumerator() << "/" << frac1.getDenominator();
+	fraction frac2(3, 4);
+	fraction frac3 = frac1 + frac2;
+	cout << "fraction: " << frac3.getNumerator() << "/" << frac3.getDenominator();
 	
 	return 0;
 }
